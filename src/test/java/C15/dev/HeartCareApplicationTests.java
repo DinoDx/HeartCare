62dbd3295eaeb866affc0190cb26c2b35291586b
Leopoldo Todisco <80098232+leotodisco@users.noreply.github.com>
2022-12-29 10:29:06 +0100

    Initial Commit

---
+++ src/test/java/C15/dev/HeartCareApplicationTests.java
@@ -0,0 +1,13 @@
+package C15.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
