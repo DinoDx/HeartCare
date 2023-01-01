9524ac13f3e4bfad20dbfcf826e14daec21a640a
AlbertBruteForce <carlovenditto28@gmail.com>
2023-01-01 16:37:46 +0100

    Added PazienteDAO and MedicoDAO

---
+++ src/main/java/c15/dev/model/dao/PazienteDAO.java
@@ -0,0 +1,12 @@
+package c15.dev.model.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author carlo
+ * creato il 1/1/2023
+ * Questa classe rappresenta il DAO della classe Paziente.
+ */
+@Repository
+public interface PazienteDAO extends UtenteRegistratoDAO{
+}
