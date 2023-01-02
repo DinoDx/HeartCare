df2880a1a15600fb9b08b8b095430e7188fcd6e1
alessaless <zoccolaaless@gmail.com>
2023-01-02 14:02:22 +0100

    Added MisurazioneDAO, MisurazioneEnzimiCardiaciDAO, MisurazionePressioneDAO and MisurazioneSaturazioneDAO

---
+++ src/main/java/c15/dev/model/dao/MisurazioneEnzimiCardiaciDAO.java
@@ -0,0 +1,8 @@
+package c15.dev.model.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MisurazioneEnzimiCardiaciDAO extends MisurazioneDAO{
+
+}
---
+++ src/main/java/c15/dev/model/dao/MisurazioneSaturazioneDAO.java
@@ -0,0 +1,7 @@
+package c15.dev.model.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MisurazioneSaturazioneDAO extends MisurazioneDAO{
+}
