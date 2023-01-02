ab81578ad90bbc66dd60596008b86dc883b91b66
Leopoldo Todisco <80098232+leotodisco@users.noreply.github.com>
2023-01-02 13:19:03 +0100

    IndirizzoDAO added.

--- src/main/java/c15/dev/model/dao/MedicoDAO.java
+++ src/main/java/c15/dev/model/dao/MedicoDAO.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Repository;
 
 
 /**
- * @author carlo
- * creato il 1/1/2023
+ * @author carlo.
+ * creato il 1/1/2023.
  * Questa classe rappresenta il DAO della classe Medico.
  */
 @Repository
