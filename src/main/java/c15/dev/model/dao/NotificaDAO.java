1c7d686067207493df3043a8533090b26bd7d665
VincenzoAr <114821721+VincenzoAr@users.noreply.github.com>
2023-01-02 15:52:37 +0100

    Added documentation to NotificaDAO and VisitaDAO

--- src/main/java/c15/dev/model/dao/NotificaDAO.java
+++ src/main/java/c15/dev/model/dao/NotificaDAO.java
@@ -4,6 +4,11 @@ import c15.dev.model.entity.Notifica;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @author Vincenzo Arnone
+ * creato il 2/1/2023
+ * Questa classe rappresenta il DAO della classe Notifica.
+ */
 @Repository
 public interface NotificaDAO extends JpaRepository<Notifica,String> {
 }
--- src/main/java/c15/dev/model/dao/VisitaDAO.java
+++ src/main/java/c15/dev/model/dao/VisitaDAO.java
@@ -4,6 +4,11 @@ import c15.dev.model.entity.Visita;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @author Vincenzo Arnone
+ * creato il 2/1/2023
+ * Questa classe rappresenta il DAO della classe Visita.
+ */
 @Repository
 public interface VisitaDAO extends JpaRepository<Visita,String> {
 }
