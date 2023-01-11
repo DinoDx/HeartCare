d1006a917ac9c2ac36495467fd6f445e3e78234f
AlbertBruteForce <carlovenditto28@gmail.com>
2023-01-11 11:16:02 +0100

    Add ModificaDatiForm, styleModificaDatiForm.

--- frontend/heartcarefrontend/src/pages/Profilo.js
+++ frontend/heartcarefrontend/src/pages/Profilo.js
@@ -1,7 +1,12 @@
 import React from "react";
+import MainContent from "../components/MainContent";
+import ModificaDatiForm from "../components/ModificaDatiForm";
 
 function Profilo() {
-  return <div>Profilo</div>;
+  return (<div className="contenitoreMainContent">
+    <MainContent/>  
+    <ModificaDatiForm/> 
+  </div>);
 }
 
 export default Profilo;
