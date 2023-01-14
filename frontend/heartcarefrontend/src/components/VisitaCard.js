6910b6b6d89fb20fde7d31abf2ba22f50c8a05da
Leopoldo Todisco <80098232+leotodisco@users.noreply.github.com>
2023-01-14 17:08:38 +0100

    add home-medico page.

---
+++ frontend/heartcarefrontend/src/components/VisitaCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { BiCalendar } from "react-icons/bi";
+import { AiOutlineClockCircle } from "react-icons/ai";
+import { MdOutlinePlace } from "react-icons/md";
+import "../css/style.css";
+import "../css/visita-style.css";
+
+function VisitaCard(props) {
+  return (
+    <div className="container-visita">
+      <span className="titolo-visita">Visita di controllo</span>
+      <hr className="linea-visita"></hr>
+
+      <div className="soggetto-visita-container">
+        <span className="nome-soggetto">Mario Rossi</span>
+      </div>
+
+      <div className="voce-visita">
+        <BiCalendar />
+        <span className="testo-voce-visita">Mercoledì 23 Dicembre</span>
+      </div>
+
+      <div className="voce-visita">
+        <AiOutlineClockCircle />
+        <span className="testo-voce-visita">10:00 - 12:00</span>
+      </div>
+
+      <div className="voce-visita">
+        <MdOutlinePlace />
+        <span className="testo-voce-visita">Studio Medico Olivia Gargiulo</span>
+      </div>
+
+      <div className="footer-visita">
+        <button className="button-visualizza-fascicolo">
+          Fascicolo Paziente
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default VisitaCard;
