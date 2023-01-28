c3dcc39b4634e04e27465f3d4491c3b5284b1715
alessaless <zoccolaaless@gmail.com>
2023-01-29 00:58:36 +0100

    Add modal for start measuring data, fix layout of pages

--- frontend/heartcarefrontend/src/pages/HomePaziente.js
+++ frontend/heartcarefrontend/src/pages/HomePaziente.js
@@ -9,9 +9,12 @@ import NoteContainer from "../components/NoteContainer";
 import VisitePazienteContainer from "../components/VisitePazienteContainer";
 import Grafico from "../components/Grafico";
 import jwt from "jwt-decode"
+import {BiPlusCircle} from "react-icons/bi";
+import {Modal} from "react-responsive-modal";
+import CardDispositivo from "../components/CardDispositivo";
+import { CiMedicalCase } from "react-icons/ci";
 
 function HomePaziente() {
-
 
     const [Categorie,setCategorie] = useState([]);
     const [Misurazioni,setMisurazioni] = useState([]);
@@ -56,15 +59,87 @@ function HomePaziente() {
     }, []);
 
 
+    const [dispositivi, setDispositivi] = useState([]);
+
+    //const [loading, setLoading] = useState(false);
+
+    const aggiornaDispositivi = (disp) => {
+        setDispositivi(disp);
+    }
+
+    let dispositivoIniziale;
+
+    const fetchDispositivi = async () => {
+        if(token == null) {
+            return;
+        }
+        const idPaziente = jwt(token)["id"];
+        return await fetch("http://localhost:8080/getDispositiviByUtente/"+idPaziente, {
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+            redirect: "follow",
+            referrerPolicy: "no-referrer",
+        }).then((response) => {
+            return response.json()
+        }).then(data => {
+            setDispositivi(data)
+        });
+    }
+
+    const [dispositivoMisurazione, setDispositivoMisurazione] = useState();
+
+    useEffect( () => {
+        fetchDispositivi();
+    }, [])
+
+
+    const [open, setOpen] = useState(false);
+    const [openRisultati, setOpenModalRisultati] = useState(false);
+
+    const onOpenModal = () => setOpen(true);
+    const onOpenModalRisultati = () => setOpenModalRisultati(true);
+    const onCloseModal = () => setOpen(false);
+    const onCloseModalRisultati = () => setOpenModalRisultati(false);
+
+    const aggiornaDispositivoMisurazione = (event) => {
+        setDispositivoMisurazione(event.target.value);
+    }
+    const avviaMisurazione = () =>{
+        // fare la fetch per inviare dati
+        console.log("stai avviando la misurazione con il dispositivo: "+dispositivoMisurazione)
+
+    }
+
     return !localStorage.getItem("token") ? (
         <Navigate to={"/Login"} />
     ) : (
         <div className="contenitorePazientiContent contenitoreHomePaziente">
-            <div className="searchbar">
-                <input id="search" type="text" placeholder=" 🔍 Cerca dottore..." />
-            </div>
-
             <span className="testo-bentornat">Bentornato, Gianluigi Buffon👋🏻</span>
+
+            <Modal open={open} onClose={onCloseModal} center>
+                <h2>Scegli il dispositivo da cui avviare la misurazione</h2>
+                <select className="selectDispositivo" onChange={(e) => aggiornaDispositivoMisurazione(e)} id="selectDispositivi">
+                    <option value="" disabled selected>I tuoi dispositivi</option>
+                    {Object.keys(dispositivi).map(function(num, index){
+                        return (
+                            <option value={dispositivi[num]["id"]}>{dispositivi[num]["categoria"]}</option>
+                        )
+                    })}
+                </select>
+                <br/>
+                <button className="bottoneInviaNota" onClick={() => {onOpenModalRisultati(); onCloseModal(); avviaMisurazione();}}>Avvia Misurazione</button>
+            </Modal>
+
+            <Modal open={openRisultati} onClose={onCloseModalRisultati} center>
+                <h2>Risultati della tua misurazione:</h2>
+            </Modal>
+
 
             <div className="contenitoreSinistra">
                 <div className="bannerNero">
@@ -83,7 +158,7 @@ function HomePaziente() {
                 </div>
                 <Grafico categorie={Categorie} misurazioni = {Misurazioni}/>
                 <div className="containerBottoni">
-                    <button className="bottoneHomePaziente">Avvio Misurazione</button>
+                    <button className="bottoneHomePaziente" onClick={onOpenModal} >Avvio Misurazione</button>
                     <button className="bottoneHomePaziente">Avvio Predizione</button>
                 </div>
 
